160a980fba95165734076215d5a7d6a23dc28191
Cristian Richard Kulessa <kulessa@gmail.com>
2022-12-06 23:16:45 -0300

    Segundo Exercício Exercise: Create the phone number translator app

--- Build mobile and desktop apps with .NET MAUI/1/Phoneword/Phoneword/MainPage.xaml.cs
+++ Build mobile and desktop apps with .NET MAUI/1/Phoneword/Phoneword/MainPage.xaml.cs
@@ -2,23 +2,47 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count+=5;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    string translatedNumber;
+    private void OnTranslate(object sender, EventArgs e)
+    {
+        string enteredNumber = PhoneNumberText.Text;
+        translatedNumber = PhonewordTranslator.ToNumber(enteredNumber);
+        if (!string.IsNullOrEmpty(translatedNumber))
+        {
+            CallButton.IsEnabled = true;
+            CallButton.Text = $"Call {translatedNumber}";
+        }
+        else
+        {
+            CallButton.IsEnabled = false;
+            CallButton.Text = "Call";
+        }
+    }
+    async void OnCall(object sender, EventArgs e)
+    {
+        if (await DisplayAlert("Dial a Number", $"Would you like to call {translatedNumber}?", "Yes", "No"))
+        {
+            try
+            {
+                if (PhoneDialer.Default.IsSupported)
+                {
+                    PhoneDialer.Default.Open(translatedNumber);
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                await DisplayAlert("Unable to dial", "Phone number is not valid.", "OK");
+            }
+            catch (Exception)
+            {
+                await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                throw;
+            }
+        }
+    }
 }
 
